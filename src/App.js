12a32592f53366b74c04bde6365f9feb8daa7eb9
ss473901 <satoshi.473901.1205@gmail.com>
2022-01-02 01:20:12 +0900

    Add Data-picker

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import { useState } from "react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/input";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
   const [events, setEvents] = useState([]);
   const [eventTitle, setEventTitle] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedDate, setSelectedDate] = useState(new Date());
   return (
-    <div>
-      <input
-        placeholder="タイトルを入力"
-        velue={eventTitle}
-        onChange={(e) => setEventTitle(e.target.value)}
-      />
+    <ChakraProvider>
+      <Button onClick={onOpen}>スケジュールを追加</Button>
       <FullCalendar
         plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
@@ -23,7 +36,43 @@ function App() {
           setEventTitle("");
         }}
       />
-    </div>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>スケジュールを追加</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Input
+              value={eventTitle}
+              onChange={(e) => setEventTitle(e.target.value)}
+              placeholder="タイトルを入力"
+            />
+            <DatePicker
+              selected={selectedDate}
+              onChange={(date) => setSelectedDate(date)}
+            />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              colorScheme="blue"
+              mr={3}
+              onClick={() => {
+                console.log(selectedDate);
+                setEvents([
+                  ...events,
+                  { title: eventTitle, date: selectedDate },
+                ]);
+                onClose();
+              }}
+            >
+              保存
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </ChakraProvider>
   );
 }
 
