51d15230dbd2ddf8d94dec5be50fe5cae3e691bf
ss473901 <satoshi.473901.1205@gmail.com>
2022-01-01 23:08:34 +0900

    Add eventTitle State

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ import { useState } from "react";
 
 function App() {
   const [events, setEvents] = useState([]);
+  const [eventTitle, setEventTitle] = useState("");
   return (
     <div>
+      <input
+        placeholder="タイトルを入力"
+        velue={eventTitle}
+        onChange={(e) => setEventTitle(e.target.value)}
+      />
       <FullCalendar
         plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
         events={events}
         dateClick={(e) => {
           const dateStr = e.dateStr;
-          setEvents([...events, { title: "テスト", date: dateStr }]);
+          setEvents([...events, { title: eventTitle, date: dateStr }]);
+          setEventTitle("");
         }}
       />
     </div>
