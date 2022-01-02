b08ca77cd27a09223363bed29dc52d4065f3af56
ss473901 <satoshi.473901.1205@gmail.com>
2022-01-03 08:46:00 +0900

    Add FullCalendar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import { useState } from "react";
+//FullCalendar
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+//chakura
+import { Button, Input, useDisclosure } from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+//datepicker
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+//date-fns
+import { format } from "date-fns";
 
 function App() {
-  const [entents, setEvents] = useState;
-  
+  const [events, setEvents] = useState([]);
+  const [eventTitle, setEventTitle] = useState("");
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <ChakraProvider>
+
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        events={events}
+      />
+    </ChakraProvider>
+  );
 }
 
 export default App;
